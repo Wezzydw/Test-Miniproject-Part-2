d7b9e587f18c67370518d7b828889d4bd8d1ab0f
mapoul <44639698+mapoul@users.noreply.github.com>
2021-10-01 14:09:26 +0200

    forskellige unittests

--- HotelBooking/HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking/HotelBooking.UnitTests/BookingManagerTests.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using HotelBooking.Core;
 using HotelBooking.UnitTests.Fakes;
+using Moq;
 using Xunit;
 
 namespace HotelBooking.UnitTests
@@ -41,5 +43,100 @@ namespace HotelBooking.UnitTests
             Assert.NotEqual(-1, roomId);
         }
 
+        [Theory]
+        [InlineData("25/10/2021", "30/10/2021")]
+        [InlineData("22/10/2021", "30/10/2021")]
+        [InlineData("12/11/2021", "20/11/2021")]
+        public void CreateBooking_FromDifDates_AssertTrue(string start, string end)
+        {
+            List<Booking> bookings = new List<Booking>
+            {
+                new Booking { Id=1, StartDate=DateTime.Today.AddDays(10), EndDate=DateTime.Today.AddDays(20), IsActive=true, CustomerId=1, RoomId=1 },
+                new Booking { Id=2, StartDate=DateTime.Today.AddDays(10), EndDate=DateTime.Today.AddDays(20), IsActive=true, CustomerId=2, RoomId=2 },
+            };
+            List<Room> rooms = new List<Room>
+            {
+                new Room { Id=1, Description="A" },
+                new Room { Id=2, Description="B" },
+            };
+            Mock<IRepository<Booking>> bookingMock = new Mock<IRepository<Booking>>();
+            Mock<IRepository<Room>> roomMock = new Mock<IRepository<Room>>();
+            bookingMock.Setup(x => x.GetAll()).Returns(bookings);
+            roomMock.Setup(x => x.GetAll()).Returns(rooms);
+
+            IBookingManager bookingManagerWithMock = new BookingManager(bookingMock.Object, roomMock.Object);
+
+
+            Booking b = new Booking();
+            
+            b.StartDate = DateTime.Parse(start);
+            b.EndDate = DateTime.Parse(end);
+
+            Assert.True(bookingManagerWithMock.CreateBooking(b));
+        }
+
+        [Theory]
+        [InlineData("10/10/2021", "15/10/2021")]
+        [InlineData("09/10/2021", "20/10/2021")]
+        [InlineData("15/10/2021", "25/10/2021")]
+        public void CreateBooking_FromDifDates_AssertFalse(string start, string end)
+        {
+            List<Booking> bookings = new List<Booking>
+            {
+                new Booking { Id=1, StartDate=DateTime.Today.AddDays(10), EndDate=DateTime.Today.AddDays(20), IsActive=true, CustomerId=1, RoomId=1 },
+                new Booking { Id=2, StartDate=DateTime.Today.AddDays(10), EndDate=DateTime.Today.AddDays(20), IsActive=true, CustomerId=2, RoomId=2 },
+            };
+            List<Room> rooms = new List<Room>
+            {
+                new Room { Id=1, Description="A" },
+                new Room { Id=2, Description="B" },
+            };
+            Mock<IRepository<Booking>> bookingMock = new Mock<IRepository<Booking>>();
+            Mock<IRepository<Room>> roomMock = new Mock<IRepository<Room>>();
+            bookingMock.Setup(x => x.GetAll()).Returns(bookings);
+            roomMock.Setup(x => x.GetAll()).Returns(rooms);
+
+            IBookingManager bookingManagerWithMock = new BookingManager(bookingMock.Object, roomMock.Object);
+
+
+            Booking b = new Booking();
+
+            b.StartDate = DateTime.Parse(start);
+            b.EndDate = DateTime.Parse(end);
+
+            Assert.False(bookingManagerWithMock.CreateBooking(b));
+        }
+
+        [Theory]
+        [InlineData("10/10/2021", "15/10/2021")]
+        [InlineData("09/10/2021", "20/10/2021")]
+        [InlineData("15/10/2021", "25/10/2021")]
+        public void CreateBooking_FromDifDates_TestException(string start, string end)
+        {
+            List<Booking> bookings = new List<Booking>
+            {
+                new Booking { Id=1, StartDate=DateTime.Today.AddDays(10), EndDate=DateTime.Today.AddDays(20), IsActive=true, CustomerId=1, RoomId=1 },
+                new Booking { Id=2, StartDate=DateTime.Today.AddDays(10), EndDate=DateTime.Today.AddDays(20), IsActive=true, CustomerId=2, RoomId=2 },
+            };
+            List<Room> rooms = new List<Room>
+            {
+                new Room { Id=1, Description="A" },
+                new Room { Id=2, Description="B" },
+            };
+            Mock<IRepository<Booking>> bookingMock = new Mock<IRepository<Booking>>();
+            Mock<IRepository<Room>> roomMock = new Mock<IRepository<Room>>();
+            bookingMock.Setup(x => x.GetAll()).Returns(bookings);
+            roomMock.Setup(x => x.GetAll()).Returns(rooms);
+
+            IBookingManager bookingManagerWithMock = new BookingManager(bookingMock.Object, roomMock.Object);
+
+
+            Booking b = new Booking();
+
+            b.StartDate = DateTime.Parse(start);
+            b.EndDate = DateTime.Parse(end);
+
+            Assert.Throws<ArgumentException>(bookingManagerWithMock.CreateBooking(b));
+        }
     }
 }
