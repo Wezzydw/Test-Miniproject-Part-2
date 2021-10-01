8d0b04eb79f0700337ab85276bd7c5fbdcdf1135
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2021-10-01 15:52:01 +0200

    Update BookingManagerTests.cs

--- HotelBooking/HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking/HotelBooking.UnitTests/BookingManagerTests.cs
@@ -138,5 +138,103 @@ namespace HotelBooking.UnitTests
 
             Assert.Throws<ArgumentException>(() => bookingManagerWithMock.CreateBooking(b));
         }
+
+        [Theory]
+        [InlineData("02/10/2021", "01/10/2021")]
+        [InlineData("03/10/2021", "01/10/2021")]
+        [InlineData("01/11/2021", "01/10/2021")]
+        public void GetFullyOccupiedDates_StartDateNotInTheFuture_ThrowsException(string start, string end)
+        {
+
+
+            Mock<IRepository<Booking>> bookingRepositoryMock = new Mock<IRepository<Booking>>();
+            Mock<IRepository<Room>> roomRepositoryMock = new Mock<IRepository<Room>>();
+
+
+            IBookingManager bookingManagerMock = new BookingManager(bookingRepositoryMock.Object, roomRepositoryMock.Object);
+
+
+            DateTime dateStart = DateTime.Parse(start);
+            DateTime dateEnd = DateTime.Parse(end);
+
+            Assert.Throws<ArgumentException>(() => bookingManagerMock.GetFullyOccupiedDates(dateStart, dateEnd));
+        }
+
+
+        [Theory]
+        [InlineData("01/10/2021", "05/10/2021")]
+        [InlineData("03/10/2021", "08/10/2021")]
+        [InlineData("01/10/2021", "03/10/2021")]
+        public void GetFullyOccupiedDates_NumberOfRoomsGreaterThanBookings_EmptyList(string start, string end)
+        {
+            DateTime dateStart = DateTime.Parse(start);
+            DateTime dateEnd = DateTime.Parse(end);
+
+
+            List<Room> rooms = new List<Room>
+            {
+                new Room { Id=1, Description="A" },
+                new Room { Id=2, Description="B" },
+                new Room { Id=3, Description="C" },
+            };
+
+            List<Booking> bookings = new List<Booking>
+            {
+                new Booking { Id=1, IsActive =  true, StartDate = dateStart,EndDate = dateEnd},
+                new Booking { Id=2, IsActive =  true, StartDate = dateStart,EndDate = dateEnd},
+            };
+
+
+            Mock<IRepository<Booking>> bookingRepositoryMock = new Mock<IRepository<Booking>>();
+            Mock<IRepository<Room>> roomRepositoryMock = new Mock<IRepository<Room>>();
+
+
+            roomRepositoryMock.Setup(x => x.GetAll()).Returns(rooms);
+            bookingRepositoryMock.Setup(x => x.GetAll()).Returns(bookings);
+
+            IBookingManager bookingManagerMock = new BookingManager(bookingRepositoryMock.Object, roomRepositoryMock.Object);
+
+            Assert.Empty(bookingManagerMock.GetFullyOccupiedDates(dateStart, dateEnd));
+
+        }
+
+
+        [Theory]
+        [InlineData("01/10/2021", "05/10/2021")]
+        [InlineData("03/10/2021", "08/10/2021")]
+        [InlineData("01/10/2021", "03/10/2021")]
+        public void GetFullyOccupiedDates_NumberOfRoomsEqualToBookings_FullList(string start, string end)
+        {
+            DateTime dateStart = DateTime.Parse(start);
+            DateTime dateEnd = DateTime.Parse(end);
+
+
+            List<Room> rooms = new List<Room>
+            {
+                new Room { Id=1, Description="A" },
+                new Room { Id=2, Description="B" },
+                new Room { Id=3, Description="C" },
+            };
+
+            List<Booking> bookings = new List<Booking>
+            {
+                new Booking { Id=1, IsActive =  true, StartDate = dateStart,EndDate = dateEnd},
+                new Booking { Id=2, IsActive =  true, StartDate = dateStart,EndDate = dateEnd},
+                new Booking { Id=3, IsActive =  true, StartDate = dateStart,EndDate = dateEnd},
+            };
+
+
+            Mock<IRepository<Booking>> bookingRepositoryMock = new Mock<IRepository<Booking>>();
+            Mock<IRepository<Room>> roomRepositoryMock = new Mock<IRepository<Room>>();
+
+
+            roomRepositoryMock.Setup(x => x.GetAll()).Returns(rooms);
+            bookingRepositoryMock.Setup(x => x.GetAll()).Returns(bookings);
+
+            IBookingManager bookingManagerMock = new BookingManager(bookingRepositoryMock.Object, roomRepositoryMock.Object);
+
+            Assert.Empty(bookingManagerMock.GetFullyOccupiedDates(dateStart, dateEnd));
+
+        }
     }
 }
